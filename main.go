7c91354a2ea0d4a1e3afdddaecffb357779ac84f
Mary Dickson Diaz <mdickson@gmail.com>
2017-01-04 19:13:16 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	var isHost bool
+
+	flag.BoolVar(&isHost, "listen", false, "listens on the specified IP address")
+
+	flag.Parse()
+
+	if isHost {
+		// go run main.go - listen <ip>
+		connIP := os.Args[2]
+		runHost(connIP)
+	} else {
+		// go run main.go <ip>
+		connIP := os.Args[1]
+		fmt.Println("is guest")
+		runGuest(connIP)
+	}
+}
+
+const port = "8080"
+
+func runHost(ip string) {
+	ipAndPort := ip + ":" + port
+	listener, listenErr := net.Listen("tcp", ipAndPort)
+	if listenErr != nil {
+		log.Fatal("Error: ", listenErr)
+	}
+
+	conn, acceptErr := listener.Accept()
+	if acceptErr != nil {
+		log.Fatal("Error: ", acceptErr)
+	}
+
+	reader := bufio.NewReader(conn)
+	message, readErr := reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error: ", readErr)
+	}
+	fmt.Println("Message received: ", message)
+}
+
+func runGuest(ip string) {
+
+}
